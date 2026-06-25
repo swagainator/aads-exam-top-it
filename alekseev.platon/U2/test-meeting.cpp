1bebe5b874c1477e38008cb80fae21ecee8fedbf
swagainator <platonalekseevi6@gmail.com>
2026-06-25 12:44:48 +0300

    test: cover U2 meeting commands
    Add focused tests for U2 meeting parsing, command behavior, and edge cases.

---
+++ alekseev.platon/U2/test-meeting.cpp
@@ -0,0 +1,60 @@
+#define BOOST_TEST_MODULE U2_MEETING
+#include <boost/test/included/unit_test.hpp>
+
+#include "meeting.hpp"
+
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+BOOST_AUTO_TEST_CASE(parse_valid_meeting_line)
+{
+  alekseev::Meeting meeting = {0, 0, 0};
+
+  BOOST_TEST(alekseev::parseMeetingLine("33 32 11", meeting));
+  BOOST_TEST(meeting.first == 33);
+  BOOST_TEST(meeting.second == 32);
+  BOOST_TEST(meeting.time == 11);
+}
+
+BOOST_AUTO_TEST_CASE(reject_invalid_meeting_lines)
+{
+  alekseev::Meeting meeting = {0, 0, 0};
+
+  BOOST_TEST(!alekseev::parseMeetingLine("", meeting));
+  BOOST_TEST(!alekseev::parseMeetingLine("33 32", meeting));
+  BOOST_TEST(!alekseev::parseMeetingLine("33 32 11 extra", meeting));
+  BOOST_TEST(!alekseev::parseMeetingLine("-33 32 11", meeting));
+  BOOST_TEST(!alekseev::parseMeetingLine("33 bad 11", meeting));
+}
+
+BOOST_AUTO_TEST_CASE(ignore_self_and_add_unknown_persons)
+{
+  const char* const filename = "/tmp/alekseev-u2-meeting-test.txt";
+  {
+    std::ofstream output(filename);
+    output << "33 33 100\n33 41 10\n";
+  }
+
+  alekseev::PersonArray persons = {nullptr, 0, 0};
+  alekseev::MeetingArray meetings = {nullptr, 0, 0};
+  alekseev::initPersonArray(persons);
+  alekseev::initMeetingArray(meetings);
+  {
+    std::ifstream input(filename);
+    BOOST_REQUIRE(alekseev::readMeetings(input, meetings, persons));
+  }
+
+  BOOST_TEST(meetings.size == 1);
+  BOOST_TEST(persons.size == 2);
+  const size_t firstIndex = alekseev::findPersonIndex(persons, 33);
+  const size_t secondIndex = alekseev::findPersonIndex(persons, 41);
+  BOOST_REQUIRE(firstIndex != persons.size);
+  BOOST_REQUIRE(secondIndex != persons.size);
+  BOOST_TEST(!alekseev::hasPersonInfo(persons.data[firstIndex]));
+  BOOST_TEST(!alekseev::hasPersonInfo(persons.data[secondIndex]));
+
+  alekseev::destroyMeetingArray(meetings);
+  alekseev::destroyPersonArray(persons);
+  std::remove(filename);
+}
